218e8a0e73c5b4f87818e33727512f18d947c8b3
Pranav <pranav.dilip.patel@gmail.com>
2017-04-17 21:37:45 +0200

    Added the JUnit Test for Test Cases
    I have added a skeleton of the Test Cases I have made for some of the Use Cases.

---
+++ MethodsTest.java
@@ -0,0 +1,34 @@
+package jUnitClasses;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class MethodsTest {
+	
+	@Test
+	public void testSaveEvent() {
+		
+	}
+	
+	@Test
+	public void testLoadTimeline() {
+		
+	}
+	
+	@Test
+	public void testSaveTimeline() {
+		
+	}
+	
+	@Test
+	public void testAddEvent() {
+		
+	}
+	
+	@Test
+	public void testEditEvent() {
+		
+	}
+
+}
