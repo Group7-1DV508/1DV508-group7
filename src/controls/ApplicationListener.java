748618e3e67b5fee17d04a7cf8ca24a5d038f220
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-04-17 15:35:34 +0200

    Added Listners

---
+++ src/controls/ApplicationListener.java
@@ -0,0 +1,10 @@
+package controls;
+
+public interface ApplicationListener {
+	
+	void onScrollVertically(); 		//vertically scroll on window
+	void onScrollHorizontally();	//horizontally scroll on window
+	void onEditColor();				//edit color of timeline
+	void onEditFont();				//edit font on timeline
+
+}
---
+++ src/controls/TimelineListener.java
@@ -0,0 +1,9 @@
+package controls;
+
+import java.time.LocalDate;
+
+public interface TimelineListener {
+	
+	void onAddTimeline(String name, LocalDate start, LocalDate end);			//add new timeline
+
+}
