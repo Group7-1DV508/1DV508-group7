06d927858d73ebb1f4f8d842d365a77aa6993863
Svampen125 <johan.e.1993@gmail.com>
2017-04-18 11:54:10 +0200

    E ventTest

--- src/test/EventTest.java
+++ src/test/EventTest.java
@@ -1,24 +1,78 @@
-package test;
-
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
+import java.time.LocalDate;
+
 import org.junit.Test;
 
 public class EventTest {
+	
+	String name = "Smoke";
+	String description = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+	LocalDate start = LocalDate.now();
+	LocalDate end = start.plusDays(2);
+	Event event = new Event(name, description, start, end);
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+	/* @Test
+	public void testEventStringStringLocalDateLocalDate() {
+		fail("Not yet implemented");
 	}
 
 	@Test
-	public void test() {
+	public void testEventStringStringLocalDate() {
 		fail("Not yet implemented");
+	} */
+
+	@Test
+	public void testGetEventName() {
+		assertTrue(event.getEventName() == name);
+		 assertEquals(event.getEventName(), name);
+	}
+
+	@Test
+	public void testSetEventName() {
+		String newName = "Anything";
+		event.setEventName(newName);
+	    assertEquals(newName, event.getEventName());
+		
+	}
+
+	@Test
+	public void testGetEventDescription() {
+		assertTrue(event.getEventDescription() == description);
+		assertEquals(event.getEventDescription(), description);
+	}
+
+	@Test
+	public void testSetEventDescription() {
+		String newDescription = "";
+		event.setEventDescription(newDescription);
+		assertEquals(newDescription, event.getEventDescription());
+	}
+
+	@Test
+	public void testGetEventStart() {
+		assertTrue(event.getEventStart() == start);
+		 assertEquals(event.getEventStart(), start);
+	}
+
+	@Test
+	public void testSetEventStart() {
+		LocalDate yesterday = start.minusDays(-1);
+		event.setEventStart(yesterday);
+		assertEquals(yesterday, event.getEventStart());
+	}
+
+	@Test
+	public void testGetEventEnd() {
+		assertTrue(event.getEventEnd() == end);
+		assertEquals(event.getEventEnd(), end);
+	}
+
+	@Test
+	public void testSetEventEnd() {
+		LocalDate aYearfromNow = start.plusDays(365);
+		event.setEventEnd(aYearfromNow);
+		assertEquals(aYearfromNow, event.getEventEnd());
 	}
 
 }
