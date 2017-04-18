4e523a0caff7886959f4b5e7f58b4a4325edfe68
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-04-18 08:01:04 +0200

    Delete ApplicationInterface.java

--- src/se/lnu/timelinemanager/ApplicationInterface.java
+++
@@ -1,9 +0,0 @@
-package se.lnu.timelinemanager;
-
-import java.time.LocalDate;
-
-public interface ApplicationInterface {
-	
-	public void addTimeline(String name, LocalDate startDate, LocalDate endDate);
-
-}
