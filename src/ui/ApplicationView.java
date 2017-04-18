b0f7f17a210258432dbc8804fea52f6a54fa505b
Caroline-Nilsson <carolinenilsson@vwifi-student-903.lnu.se>
2017-04-18 10:52:46 +0200

    uppsate AppView

--- src/ui/ApplicationView.java
+++ src/ui/ApplicationView.java
@@ -9,8 +9,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
-//import functions.ApplicationFunction;
-
 import functions.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -26,23 +24,6 @@ public class ApplicationView {
 	private TimelineView timelineView;
 	private ApplicationListener appListener;
 	
-	//private static ApplicationFunction af = null;
-	
-
-
-	public TextField tF1 = new TextField();
-	public TextField tF2 = new TextField();
-	public TextField tF3 = new TextField();
-	public TextField tF4 = new TextField();
-	public TextField tF5 = new TextField();
-	public TextField tF6 = new TextField();
-	public TextField tF7 = new TextField();
-	public VBox root = new VBox();
-	public VBox holder2 = new VBox();
-	public VBox root2 = new VBox();
-	public Scene scene;
-	public Scene scene1;
-	public Stage stage;
 
 	
 	public ApplicationView() {
@@ -62,36 +43,14 @@ public class ApplicationView {
 		return timelineView;
 	}
 
-
-	//public static void setApplicationFunction(ApplicationFunction af) {
-		//ApplicationView.af = af;
-	//}
-
-
-	public VBox create() {
-		VBox holder = new VBox();
-		List<Timeline> timeLines = af.getTimelines();
-		for (Timeline tl : timeLines) {
-			Text name = new Text(20, 50, tl.getName() + ": ");
-			Text start = new Text(20, 50, "" + tl.getStart() + "--");
-			Text end = new Text(20, 50, "" + tl.getEnd());
-
-			HBox root = new HBox();
-			root.getChildren().add(name);
-			root.getChildren().add(start);
-			root.getChildren().add(end);
-			holder.getChildren().add(root);
-		}
-		return holder;
-	}
-
-
 	
 	public void start(Stage primaryStage) throws Exception {
-		VBox v = create();
+		VBox root = new VBox();
+		root.getChildren().addAll(eventView.getRoot(), timelineView.getRoot());
+		
 		primaryStage.setTitle("Timelines Panel");
-		Scene se = new Scene(v, 550, 550);
-		primaryStage.setScene(se);
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 }
