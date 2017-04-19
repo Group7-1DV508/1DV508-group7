f2ee699a6668367171aacfcc8a2a3faf9d358333
Indre Kvedaraite <kvedaraite.indre@gmail.com>
2017-04-19 10:24:42 +0200

    Change onAddEventDuration to boolean, add Johan's tests

--- src/test/TimelineControlTest.java
+++ src/test/TimelineControlTest.java
@@ -2,12 +2,20 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TimelineControlTest {
+import functions.Timeline;
 
+public class TimelineControlTest {
+	String tIsCorrect = "A";
+	LocalDateTime start = LocalDateTime.now();
+	LocalDateTime end = start.plusDays(90);
+	Timeline t = new Timeline(tIsCorrect, start, end);
 	@Before
 	public void setUp() throws Exception {
 	}
@@ -16,9 +24,32 @@ public class TimelineControlTest {
 	public void tearDown() throws Exception {
 	}
 
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testisNameCorrect() {
+		
+		assertEquals(t.getName(), tIsCorrect);
+		
 	}
+	
+	@Test
+	public void testisStartCorrect() {
+		assertEquals(t.getStart(), start);
+	}
+	
+	@Test
+	public void testisEndCorrect() {
+		assertEquals(t.getEnd(), end);
+	}
+	
+	@Test
+	public void testisCorrectInput() {
+		String wrong ="";
+		Timeline z = new Timeline(wrong, start, end);
+		assertEquals(z.getName(), wrong);
+		
+	}
+	
+	
 
 }
