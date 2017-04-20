436da59d1c2cc574bb8bd9e482b0d098a8591a34
Indre Kvedaraite <kvedaraite.indre@gmail.com>
2017-04-20 19:41:46 +0200

    Add edit event tests, implement application test, fix other tests

--- src/test/EventControlTest.java
+++ src/test/EventControlTest.java
@@ -2,50 +2,76 @@ package test;
 
 import static org.junit.Assert.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import controls.ApplicationControl;
+import controls.ChangeListener;
 import controls.EventControl;
 import functions.App;
+import functions.Timeline;
+import io.FileHandler;
+import ui.ApplicationView;
 
 public class EventControlTest {
-
+	
+	String name2;
+	String des;
+	LocalDateTime start2;
+	LocalDateTime end2;
+	EventControl eventC;
+	App app;
+	
 	@Before
 	public void setUp() throws Exception {
-	}
+		name2 = "a";
+		des = "b";
+		start2 = LocalDateTime.now();
+		end2 = start2.plusDays(30);
+		eventC = new EventControl();
+		app = new App();
+		eventC.setApp(app);
+		// Skeleton for ChangeListener interface to be able to
+		// add it to App object.
+		class ChangeListenerForTest implements ChangeListener {
+			@Override
+			public void onChangedTimeline(ArrayList<Timeline> timelines, Timeline current) {
+			}
 
-	@After
-	public void tearDown() throws Exception {
-	}
+			@Override
+			public void onNewTimelineSelected(Timeline current) {
+				
+			}
 
-/*	@Test
-	public void test() {
-		fail("Not yet implemented"); */
-//	}
+			@Override
+			public void onEditTimeline(Timeline current) {
+				
+			}
+			
+		}
+		app.addListener(new ChangeListenerForTest());
+		app.addTimeline(name2, start2, start2);
+	}
 	
 	@Test 
 	public void testonAddEventDuration() {
-		String name2 = "a";
-		String des = "b";
-		LocalDateTime start2 = LocalDateTime.now();
-		LocalDateTime end2 =  start2.plusDays(300);
-		EventControl eventC = new EventControl();
 		assertTrue(eventC.onAddEventDuration(name2, des, start2, end2)); 
 	}
 	
 	@Test
 	public void testonAddEvent(){
-		String name2 = "a";
-		String des = "b";
-		LocalDateTime start2 = LocalDateTime.now();
-		EventControl eventC = new EventControl();
-		App app = new App();
-		eventC.setApp(app);
-		app.addTimeline(name2, start2, start2);
 		assertTrue(eventC.onAddEvent(name2, des, start2));
-		
-
+	}
+	
+	@Test
+	public void testonEditEvent() {
+		assertTrue(eventC.onEditEvent(name2, des, start2));
+	}
+	
+	@Test
+	public void testonEditEventDuration() {
+		assertTrue(eventC.onEditEventDuration(name2, des, start2, end2));
 	}
 }
