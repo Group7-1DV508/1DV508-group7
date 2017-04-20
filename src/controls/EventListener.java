6cf74a4d87eda106d61bddd63724f8fe87fc17f1
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-04-20 02:51:35 +0200

    update eventView

--- src/controls/EventListener.java
+++ src/controls/EventListener.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 
 public interface EventListener {
 	
-	void onAddEventDuration(String name, String description, LocalDateTime start, LocalDateTime end);	//add event with a duration 
+	boolean onAddEventDuration(String name, String description, LocalDateTime start, LocalDateTime end);	//add event with a duration 
 	boolean onAddEvent(String name, String description, LocalDateTime start);							//add event without a duration
 	
 
