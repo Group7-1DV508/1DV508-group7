9e9bbbff53691c47250245fee924b44806f1487c
Indre Kvedaraite <kvedaraite.indre@gmail.com>
2017-04-20 18:55:01 +0200

    Add comments on TimelineControl, TimelineListener, TimelineView, fix EventTest

--- src/controls/TimelineListener.java
+++ src/controls/TimelineListener.java
@@ -4,6 +4,13 @@ import java.time.LocalDateTime;
 
 public interface TimelineListener {
 	
+	/**
+	 * Handles adding of a timeline
+	 * @param name Name of a timeline
+	 * @param start Start date of a timeline
+	 * @param end End date of a timeline
+	 * @return true if timeline has been successfully added
+	 */
 	boolean onAddTimeline(String name, LocalDateTime start, LocalDateTime end);
 	
 
