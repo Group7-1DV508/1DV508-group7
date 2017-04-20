3a8428968e252a131ed90537c18c64f1e641a8bb
Caroline-Nilsson <carolinenilsson@vwifi-student-766.lnu.se>
2017-04-20 11:15:37 +0200

    .

--- src/controls/ChangeListener.java
+++ src/controls/ChangeListener.java
@@ -6,13 +6,11 @@ import functions.Timeline;
 
 public interface ChangeListener {
 	
-	void onChangedTimeline(ArrayList<Timeline> timelines, Timeline current);		//used when a timeline is added or deleted
+	void onChangedTimeline(ArrayList<Timeline> timelines, Timeline current);//used when a timeline is added or deleted
 	
-	void onNewTimelineSelected(Timeline current);				//used when current timeline is updated
+	void onNewTimelineSelected(Timeline current);							//used when current timeline is updated
 	
-	void onEditTimeline(Timeline current);						//used when current timeline is edited, either timeline or event
-																//or when new events are created or deleted
+	void onEditTimeline(Timeline current);									//used when current timeline is edited, either timeline or event
+																			//or when new events are created or deleted
 	
-	
-
 }
