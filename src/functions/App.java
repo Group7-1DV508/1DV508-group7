a8993de696763495e6f5f4708dfb87c1c38a2b2d
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-04-20 19:41:24 +0200

    Add: Comments - App

--- src/functions/App.java
+++ src/functions/App.java
@@ -11,6 +11,9 @@ public class App {
 	private ArrayList<Timeline> timelines;
 	private Timeline current;
 	
+	/**
+	 * Constructor, initializes and ArrayList<Timeline>
+	 */
 	public App() {
 		timelines = new ArrayList<Timeline>();
 	}
@@ -28,23 +31,58 @@ public class App {
 		changeListener.onChangedTimeline(timelines, current);
 	}
 	
+	/**
+	 * Adds an Event to current timeline by calling the addEvent method in Timeline
+	 * @param name, event name
+	 * @param description , event description
+	 * @param start , event start date
+	 */
 	public void addEventToCurrent(String name, String description, LocalDateTime start) {
 		current.addEvent(name, description, start);
 		changeListener.onEditTimeline(current);
 	}
 	
+	/**
+	 * Adds an Event with duration to current timeline by calling the addEvent method in Timeline
+	 * @param name, event name
+	 * @param description , event description
+	 * @param start , event start date
+	 * @param end , event end date
+	 */
+	public void addEventToCurrentDuration(String name, String description, LocalDateTime start, LocalDateTime end) {
+		current.addEventDuration(name, description, start, end);
+		changeListener.onEditTimeline(current);
+	}
+	
+	/**
+	 * Update the ChangeListener variable with the ChangeListener given as input
+	 * @param cl , (ChangeListener)
+	 */
 	public void addListener(ChangeListener cl) {
 		changeListener = cl;
 	}
 	
+	/**
+	 * update the variable current with a new current Timeline
+	 * @param t , the Timeline used at the moment
+	 */
 	public void setCurrentTimeline(Timeline t) {
 		current = t;
 		changeListener.onNewTimelineSelected(current);
 	}
+	
+	/**
+	 * returns the currently open timeline
+	 * @return Timeline
+	 */
 	public Timeline getCurrentTimeline() {
 		return current;
 	}
 
+	/**
+	 * returns a list of all open timelines in the App
+	 * @return ArrayList<Timeline>
+	 */
 	public ArrayList<Timeline> getTimelines(){
 		return timelines;
 	}
