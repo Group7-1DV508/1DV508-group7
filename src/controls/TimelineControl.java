a2d58460d39b325ecd4d6b7df3653e98dc8077e5
Indre Kvedaraite <kvedaraite.indre@gmail.com>
2017-04-20 18:55:01 +0200

    Add comments on TimelineControl, TimelineListener, TimelineView, fix EventTest

--- src/controls/TimelineControl.java
+++ src/controls/TimelineControl.java
@@ -6,6 +6,7 @@ import functions.App;
 
 public class TimelineControl implements TimelineListener {
 	
+	
 	private App currentApp;
 
 	@Override
@@ -19,6 +20,11 @@ public class TimelineControl implements TimelineListener {
 		}
 	}
 	
+	/**
+	 * Checks if while creating a timeline a name has been chosen.
+	 * @param name Name of timeline
+	 * @return true if name is not a null
+	 */
 	private boolean isNameCorrect(String name) {
 		if (name.length() == 0) {
 			System.out.println("Please chose a name for your timeline");
@@ -29,6 +35,11 @@ public class TimelineControl implements TimelineListener {
 		}
 	}
 	
+	/**
+	 * Checks if while creating a timeline a start date has been chosen.
+	 * @param start Date of when timeline starts
+	 * @return true if start date is not a null
+	 */
 	private boolean isStartCorrect(LocalDateTime start) {
 		if (start == null) {
 			System.out.println("Please chose a start date for your timeline");
@@ -39,6 +50,11 @@ public class TimelineControl implements TimelineListener {
 		}
 	}
 	
+	/**
+	 * Checks if while creating a timeline an end date has been chosen.
+	 * @param end Date of when timeline ends
+	 * @return true if end date is not a null
+	 */
 	private boolean isEndCorrect (LocalDateTime end) {
 		if (end == null) {
 			System.out.println("Please chose an end date for your timeline");
@@ -49,6 +65,13 @@ public class TimelineControl implements TimelineListener {
 		}
 	}
 	
+	/**
+	 * Checks if all input made by user is correct
+	 * @param name Name of timeline
+	 * @param start Date of when timeline starts 
+	 * @param end Date of when timeline ends
+	 * @return
+	 */
 	private boolean isCorrectInput(String name, LocalDateTime start, LocalDateTime end) {
 		if (isNameCorrect(name) && isStartCorrect(start) && isEndCorrect(end)) {
 			return true;
@@ -58,6 +81,10 @@ public class TimelineControl implements TimelineListener {
 		}
 	}
 	
+	/**
+	 * Sets an application for this listener
+	 * @param app
+	 */
 	public void setApp(App app) {
 		currentApp = app;
 	}
