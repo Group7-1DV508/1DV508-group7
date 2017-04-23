3313c66ac6dfb20eda49c10a3cae8fa7d5bb0218
Caroline-Nilsson <carolinenilsson@Carolines-MacBook-Pro.local>
2017-04-23 16:27:13 +0200

    Add: showYear() , ComboBox show name

--- src/ApplicationMain.java
+++ src/ApplicationMain.java
@@ -31,10 +31,20 @@ public class ApplicationMain extends Application {
 		
 		appControl.setUpListeners();
 		
+		app.addTimeline("New Timeline", LocalDateTime.of(2001, 01, 01, 00, 00), LocalDateTime.of(2005, 01, 01, 00, 00));
 		app.addTimeline("TheTimeline", LocalDateTime.of(2000, 01, 01, 00, 00), LocalDateTime.of(2002, 01, 01, 00, 00));
 		app.addEventToCurrentDuration("MyEvent", "hi", LocalDateTime.of(2000, 04, 13, 00, 00), LocalDateTime.of(2000, 04, 30, 00, 00));
 		app.addEventToCurrentDuration("MyEvent", "hi", LocalDateTime.of(2000, 12, 15, 00, 00), LocalDateTime.of(2000, 12, 30, 00, 00));
 		app.addEventToCurrentDuration("MyEvent", "hi", LocalDateTime.of(2000, 05, 15, 00, 00), LocalDateTime.of(2000, 05, 30, 00, 00));
+		app.addTimeline("Timeline", LocalDateTime.of(2001, 01, 01, 00, 00), LocalDateTime.of(2005, 01, 01, 00, 00));
+		app.addEventToCurrentDuration("MyEvent", "hi", LocalDateTime.of(2001, 04, 13, 00, 00), LocalDateTime.of(2000, 04, 30, 00, 00));
+		app.addEventToCurrent("MyEvent", "hi", LocalDateTime.of(2001, 04, 20, 00, 00));
+		app.addEventToCurrent("MyEvent", "hi", LocalDateTime.of(2001, 04, 30, 00, 00));
+		app.addEventToCurrentDuration("MyEvent", "hi", LocalDateTime.of(2001, 04, 13, 00, 00), LocalDateTime.of(2000, 04, 30, 00, 00));
+		app.addEventToCurrent("MyEvent", "hi", LocalDateTime.of(2001, 04, 11, 00, 00));
+		app.addEventToCurrent("MyEvent", "hi", LocalDateTime.of(2001, 04, 15, 00, 00));
+		app.addEventToCurrent("MyEvent", "hi", LocalDateTime.of(2001, 04, 05, 00, 00));
+		app.addEventToCurrent("MyEvent", "hi", LocalDateTime.of(2001, 04, 22, 00, 00));
 
 		//Collect Root from ApplicationView and build
 		Scene scene = new Scene(appView.getRoot(), 900, 900);
