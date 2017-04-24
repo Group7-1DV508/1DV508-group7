2f4d5996e243e74fb1d0637c2752618848b1da1e
Stefan <sb223cd@student.lnu.se>
2017-04-24 15:08:15 +0200

    Delete displayTimeline

--- src/ui/TimelineView.java
+++ src/ui/TimelineView.java
@@ -9,6 +9,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
@@ -64,6 +66,11 @@ public class TimelineView {
 		gp.add(timelineBox, 0, 2);
 		addTimelineButton.setPadding(new Insets(10));
 		return gp;
+	}
+
+	public Button getAddTimelineButton(){
+
+		return addTimeline;
 	}
 
 	/**
@@ -130,6 +137,7 @@ public class TimelineView {
 		addTimelineRoot.add(dates, 0, 2);
 		addTimelineRoot.add(confirmTimelineButton, 0, 3);
 		addTimelineRoot.setAlignment(Pos.TOP_CENTER);
+
 		return addTimelineRoot;
 	}
 
@@ -139,120 +147,9 @@ public class TimelineView {
 	 */
 	private void displayTimeline() {
 
-		Separator s0 = new Separator();
-		s0.setOrientation(Orientation.VERTICAL);
-		s0.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-
-		Separator s1 = new Separator();
-		s1.setOrientation(Orientation.VERTICAL);
-		s1.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-
-		Separator s2 = new Separator();
-		s2.setOrientation(Orientation.VERTICAL);
-		s2.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-
-		Separator s3 = new Separator();
-		s3.setOrientation(Orientation.VERTICAL);
-		s3.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-
-		Separator s4 = new Separator();
-		s4.setOrientation(Orientation.VERTICAL);
-		s4.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-
-		Separator s5 = new Separator();
-		s5.setOrientation(Orientation.VERTICAL);
-		s5.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-
-		Separator s6 = new Separator();
-		s6.setOrientation(Orientation.VERTICAL);
-		s6.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-
-		Separator s7 = new Separator();
-		s7.setOrientation(Orientation.VERTICAL);
-		s7.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-
-		Separator s8 = new Separator();
-		s8.setOrientation(Orientation.VERTICAL);
-		s8.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-
-		Separator s9 = new Separator();
-		s9.setOrientation(Orientation.VERTICAL);
-		s9.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-
-		Separator s10 = new Separator();
-		s10.setOrientation(Orientation.VERTICAL);
-		s10.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-
-		Separator s11 = new Separator();
-		s11.setOrientation(Orientation.VERTICAL);
-		s11.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-
-		Separator s12 = new Separator();
-		s12.setOrientation(Orientation.VERTICAL);
-		s12.setStyle("-fx-font: 20 arial; -fx-base: #FFFFFF;");
-		s12.setTranslateX(3);
-
-		Label l1 = new Label("JAN");
-		l1.setTextFill(Color.web("#FFFFFF"));
-		l1.setFont(new Font("Times New Roman", 20));
-
-		Label l2 = new Label("FEB");
-		l2.setTextFill(Color.web("#FFFFFF"));
-		l2.setFont(new Font("Times New Roman", 20));
-
-		Label l3 = new Label("MAR");
-		l3.setTextFill(Color.web("#FFFFFF"));
-		l3.setFont(new Font("Times New Roman", 20));
-
-		Label l4 = new Label("APR");
-		l4.setTextFill(Color.web("#FFFFFF"));
-		l4.setFont(new Font("Times New Roman", 20));
-
-		Label l5 = new Label("MAY");
-		l5.setTextFill(Color.web("#FFFFFF"));
-		l5.setFont(new Font("Times New Roman", 20));
-
-		Label l6 = new Label("JUN");
-		l6.setTextFill(Color.web("#FFFFFF"));
-		l6.setFont(new Font("Times New Roman", 20));
-
-		Label l7 = new Label("JUL");
-		l7.setTextFill(Color.web("#FFFFFF"));
-		l7.setFont(new Font("Times New Roman", 20));
-
-		Label l8 = new Label("AUG");
-		l8.setTextFill(Color.web("#FFFFFF"));
-		l8.setFont(new Font("Times New Roman", 20));
-
-		Label l9 = new Label("SEP");
-		l9.setTextFill(Color.web("#FFFFFF"));
-		l9.setFont(new Font("Times New Roman", 20));
-
-		Label l10 = new Label("OCT");
-		l10.setTextFill(Color.web("#FFFFFF"));
-		l10.setFont(new Font("Times New Roman", 20));
-
-		Label l11 = new Label("NOV");
-		l11.setTextFill(Color.web("#FFFFFF"));
-		l11.setFont(new Font("Times New Roman", 20));
-
-		Label l12 = new Label("DEC");
-		l12.setTextFill(Color.web("#FFFFFF"));
-		l12.setFont(new Font("Times New Roman", 20));
-
-		HBox timeline = new HBox(15);
-
-		timeline.getChildren().addAll(s0, l1, s1, l2, s2, l3, s3, l4, s4, l5, s5, l6, s6, l7, s7, l8, s8, l9, s9, l10,
-				s10, l11, s11, l12, s12);
-		timeline.setMinHeight(50);
-		timeline.setAlignment(Pos.CENTER);
-		timeline.setStyle("-fx-background-color:#9400d3");
-		timeline.setTranslateX(10);
-		eventButton.setPadding(new Insets(5));
-
-		timelineBox.getChildren().addAll(timeline, eventButton);
 
 	}
+
 
 	/**
 	 * Private class of EventHandler that runs a method to open add timeline
@@ -268,6 +165,7 @@ public class TimelineView {
 			openAddTimeline();
 		}
 	}
+
 
 	/**
 	 * Private class of EventHandler that runs a method to save timeline in
