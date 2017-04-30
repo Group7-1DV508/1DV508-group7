6c3a36df394cc46581ddc80e751929ff820c3642
AyaKathem <to_ta575@yahoo.com>
2017-05-01 00:17:04 +0200

    alert

---
+++ src/ui/Alerts.java
@@ -0,0 +1,41 @@
+package ui;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+
+public class Alerts {
+	
+	Alert alert = new Alert(AlertType.ERROR);
+	
+public boolean Checkname ( String name, String description){
+	
+	
+	
+	
+	if (name.length() > 30){
+		
+		alert.setTitle("Erro");
+		alert.setContentText("Plase, ");
+		alert.showAndWait();
+		return false;}	
+	else if ( description.length() > 40){
+		alert.setTitle("Error");
+		alert.setContentText("Plase, dec");
+		alert.showAndWait();
+		return false;
+	}else{
+		return true;
+	}
+	
+	//}else if (year.length() > 4){
+		
+		//return false ;
+	//}
+	
+
+}
+	
+	
+
+}
