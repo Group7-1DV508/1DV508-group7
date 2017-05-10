7db509d0f6d50c38b62964ea7cf1527e59d9cda4
Caroline-Nilsson <cn222nd@student.lnu.se>
2017-05-10 03:48:09 +0200

    Update: year/month/dayView, eventCircles yearView

--- src/ui/timelineVisuals/MonthView.java
+++ src/ui/timelineVisuals/MonthView.java
@@ -8,22 +8,31 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class MonthView extends HBox {
 
-	private ArrayList<DayView> days;
-	private ArrayList<Event> events;
+
 	private final int BOX_HEIGHT = 70;
 	private final int BOX_LENGTH = 150;
+	private final Text text;
 	
 	public MonthView() {
 		setMinSize(BOX_LENGTH, BOX_HEIGHT);
 		setMinSize(BOX_LENGTH, BOX_HEIGHT);
 		setAlignment(Pos.CENTER);
 		setBackground(new Background(new BackgroundFill(Color.BLUEVIOLET, null, null)));
+		text = new Text();
+		getChildren().add(text);
 	}
 	public int getLength() {
 		return BOX_LENGTH;
+	}
+	public void setText(String str) {
+		text.setFill(Color.WHITE);
+		text.setFont(Font.font ("Times New Roman", 25));
+		text.setText(str);
 	}
 	
 }
