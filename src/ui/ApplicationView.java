9b4b36389e4f2168c2b44edd6b631bf8c0f3db09
amelow <amelielowe96@gmail.com>
2017-05-20 20:59:56 +0200

    Help for Delete event

--- src/ui/ApplicationView.java
+++ src/ui/ApplicationView.java
@@ -230,7 +230,9 @@ public class ApplicationView implements ChangeListener {
 					}
 						String hej2 = comboBox.getSelectionModel().getSelectedItem();
 							if(hej2 == "Delete Event"){
-								helpText.setText("Text to be added");
+								helpText.setText("Click on the Event circle, which should be deleted.\n"
+										+ "The event information box will appear.\n"
+										+ "Click on the “ Delete” button in the bottom right corner, next to the “Edit information” button.");
 						
 						}
 						
